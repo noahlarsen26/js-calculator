e96ffbad2a63eb76a76934dd3d1259cf292a5111
abc <abc@example.com>
2022-05-22 11:22:06 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,104 @@
+const btns = document.querySelectorAll(".btn");
+const display = document.querySelectorAll(".display");
+const answerContainer = document.querySelector(".answer-container");
+const btnContainer = document.querySelector(".btn-container");
+const divide = document.getElementById("divide");
+const addition = document.getElementById("plus");
+const subtract = document.getElementById("minus");
+const multiply = document.getElementById("times");
+const operators = document.querySelectorAll(".operator");
+const numbers = document.querySelectorAll(".number");
+const equal = document.getElementById("equal");
+const clear = document.getElementById("clear");
+const decimal = document.getElementById("decimal");
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let decimalCount = 0;
+
+numbers.forEach((num) => {
+  let value = num.textContent;
+  num.addEventListener("click", () => {
+    if (num.textContent === ".") {
+      decimalCount++;
+    }
+    if (num.textContent === "." && decimalCount > 1) {
+      return;
+    }
+    if (clear) {
+      decimalCount = 0;
+    }
+    if (operator === "") {
+      // Read first number if no operator set yet
+      answerContainer.textContent = firstNum += value;
+    } else {
+      //read second number
+      answerContainer.textContent = secondNum += value;
+    }
+  });
+});
+
+operators.forEach((op) => {
+  op.addEventListener("click", (e) => {
+    if (e.target.textContent !== "=") {
+      operator = e.target.textContent;
+
+      answerContainer.textContent = firstNum + operator;
+
+      console.log(firstNum);
+      console.log(operator);
+    } else {
+      console.log(secondNum);
+      answerContainer.textContent = secondNum;
+
+      switch (operator) {
+        case "+":
+          console.log(
+            (parseFloat(firstNum) + parseFloat(secondNum)).toFixed(2)
+          );
+          answerContainer.textContent = (
+            parseFloat(firstNum) + parseFloat(secondNum)
+          ).toFixed(2);
+          break;
+        case "-":
+          console.log(
+            (parseFloat(firstNum) - parseFloat(secondNum)).toFixed(2)
+          );
+          answerContainer.textContent = (
+            parseFloat(firstNum) - parseFloat(secondNum)
+          ).toFixed(2);
+          break;
+        case "X":
+          console.log(
+            (parseFloat(firstNum) * parseFloat(secondNum)).toFixed(2)
+          );
+          answerContainer.textContent = (
+            parseFloat(firstNum) * parseFloat(secondNum)
+          ).toFixed(2);
+          break;
+        case "/":
+          console.log(
+            (parseFloat(firstNum) / parseFloat(secondNum)).toFixed(2)
+          );
+          answerContainer.textContent = (
+            parseFloat(firstNum) / parseFloat(secondNum)
+          ).toFixed(2);
+          break;
+        default:
+          break;
+      }
+    }
+  });
+});
+
+clear.addEventListener("click", () => {
+  firstNum = "";
+  secondNum = "";
+  operator = "";
+  answerContainer.textContent = "";
+});
+
+// decimal.addEventListener("click", () => {
+//   parseInt(decimal);
+// });
